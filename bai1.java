619d66218f3b2e179e14a721be97d7e5eda178c2
HoangDung05 <88196069+HoangDung05@users.noreply.github.com>
2024-01-04 01:05:28 +0700

    Add files via upload

---
+++ bai1.java
@@ -0,0 +1,20 @@
+public class bai1 {
+    public static void result(String b, String c) {
+        System.out.println(b + " " + c);
+    }
+
+    public static void main(String[] args) {
+        // bai1
+        System.out.println("Hello World");
+        // bai2
+        String a = "Hello World";
+        System.out.println(a);
+        // bai3
+        String b = "Hello";
+        String c = "World";
+        System.out.println(b + " " + c);
+        // bai4
+        result(b, c);
+
+    }
+}
